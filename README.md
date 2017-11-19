dffd0e10071b233091f21fc08e5278cb17ec8dbc
Pavel Koch <kouks.koch@gmail.com>
2017-11-19 02:28:06 +0000

    cosmetics

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Vue Frontend Skeleton
 > Pavel Koch
 
+## Installation
+Clone the repo to desired location, rename, change package.json specs and run `npm install`.
+
 ## Commands
 `npm run build`
 
--- test/specs/App.spec.js
+++ test/specs/App.spec.js
@@ -1,15 +1,12 @@
-import Chai from 'chai'
-
 import Vue from 'vue'
-
+import { expect } from 'chai'
 import App from '../../src/components/Hello.vue'
 
 describe('App.vue', () => {
   it('should print hello world.', () => {
     const Compoment = Vue.extend(App)
-
     const vm = new Compoment().$mount()
 
-    Chai.expect(vm.$el.querySelector('p').textContent).to.equal('Hello World')
+    expect(vm.$el.querySelector('p').textContent).to.equal('Hello World')
   })
 })
